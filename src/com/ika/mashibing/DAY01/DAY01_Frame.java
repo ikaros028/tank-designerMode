9a557cbc390ef0ecfc5358e33eb3c3fc35c9839a
ikaros028 <ikaroschen028@gmail.com>
2021-08-15 21:10:47 +0800

    change to use the Frame class defining by user, call method named repaint to create animation effects

--- src/com/ika/mashibing/DAY01/DAY01_Frame.java
+++ src/com/ika/mashibing/DAY01/DAY01_Frame.java
@@ -7,33 +7,20 @@ import java.awt.event.WindowEvent;
 public class DAY01_Frame {
 
     public static void main(String[] args) {
-	    // Frame类
-        Frame f = new Frame();
-        // 设置宽高，像素
-        f.setSize(800, 600);
-        // 设置大小可变
-        f.setResizable(true);
-        // 设置标题
-        f.setTitle("tank war");
-        // 设置窗口监听器，重写关闭窗口的方法
-        f.addWindowListener(new WindowAdapter() {
-            /**
-             * 窗口关闭事件
-             * @param e
-             */
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-        // 显示窗口
-        f.setVisible(true);
-
-
         // 创建继承了Frame的用户自定义窗口对象
         DAY01_TankFrame user_T = new DAY01_TankFrame();
         user_T.setVisible(true);
-        user_T.setVisible(false);
-        user_T.setVisible(true);
+        while (true) {
+            // 线程停歇100ms
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            user_T.repaint();
+        }
+
+        // 通过循环调用，产生自动变化
+
     }
 }
--- src/com/ika/mashibing/DAY01/DAY01_TankFrame.java
+++ src/com/ika/mashibing/DAY01/DAY01_TankFrame.java
@@ -1,6 +1,8 @@
 package com.ika.mashibing.DAY01;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -11,6 +13,7 @@ public class DAY01_TankFrame extends Frame {
         setSize(800, 600);
         setResizable(false);
         setTitle("u_tank war");
+        // 内部类监听
         addWindowListener(new WindowAdapter() {
             /**
              * 窗口关闭事件
@@ -21,8 +24,38 @@ public class DAY01_TankFrame extends Frame {
                 System.exit(0);
             }
         });
+        // 添加按键监听事件，键盘触发变化事件
+        addKeyListener(new myKeyListener());
     }
 
+    /**
+     * 自定义按键监听事件
+     */
+    class myKeyListener extends KeyAdapter {
+        /**
+         * Invoked when a key has been pressed.
+         *
+         * @param e
+         */
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("the key was pressed : " + e.getKeyCode());
+            x += 20;
+            // 调用重绘方法，由重绘方法调用paint
+            repaint();
+        }
+
+        /**
+         * Invoked when a key has been released.
+         *
+         * @param e
+         */
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("the key was release : " + e.getKeyCode());
+
+        }
+    }
     /**
      * 重写窗口绘制方法，当窗口初次展示或再次展示时，会执行当前方法
      * @param g
